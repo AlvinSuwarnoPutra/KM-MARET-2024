49b121fe6512f20a8494c0e9d5137b4ea7e2b2be
alvinsuwarnoputra <alvin1234putra@webmail.umm.ac.id>
2024-03-19 21:45:37 +0700

    jawaban alvin

---
+++ js-basic/algoritma.js
@@ -0,0 +1,61 @@
+// ALGORITMA PSEUDOCODE
+
+/*
+STUDY CASE 1 
+
+Tentukan Algoritma dan Pseudocode ountuk menghitung keliling persegi panjang
+- Algoritma
+1. Buat variable panjang
+2. Buat variable lebar
+3. Buat variable keliling
+4. Masukkan rumus keliling = 2 x (panjang + lebar)
+5.  Cetak Keliling
+
+- Pseudocode
+1. STORE panjang WITH ANY NUMBER
+2. STORE lebar WITH ANY NUMBER
+
+3. STORE keliling WITH 2 MUL panjang ADD 2 MUL lebar
+4. DISPLAY KELILING
+
+
+STUDY CASE 2
+Luas Permukaan Tabung
+
+- Algoritma
+1. Cari luas permukaan tabung = 2 x 22/7 x r x r + 2 x pi x r x t
+2. Buat varibale pi
+3. buat variable r
+4. buat variable t
+5. buat variable luasPermukaan
+6. Isi luasPermukaan dengan rumus = 2 x pi x r x r + 2 x pi x r x t
+7. Cetak permukaan
+
+- Pseudocode
+1. STORE pi WITH 3.14
+2. Store variable r WITH ANY NUMBER
+3. Store variable t WITH ANY NUMBER
+
+4. STORE luasPermukaan with 2 MUL pi MUL r MUL r ADD 2 MUL pi MUL r MUL t
+5. DISPLAY luasPermukaan
+
+STUDY CASE 3
+Volume Bola
+
+- Algoritma
+1. Cari rumus volume bola = 4/3 x pi x r x r x r
+2. Buat varibale pi
+3. Buat bariable r
+4. Buat variable volume
+5. Isi volume dengan rumus = 1/4 x 4/3 x pi x r x r x r
+6. Cetak Volume
+
+- Pseudocode 
+1. Store Pi WITH 3.14
+2. STORE variable r WITH ANY NUMBER
+3. STORE variable t WITH ANY NUMBER
+
+4. STORE Volume WITH 1/4 MUL 4/3 MUL pi MUL r MUL r MUL r 
+5. Display Volume
+
+*/
